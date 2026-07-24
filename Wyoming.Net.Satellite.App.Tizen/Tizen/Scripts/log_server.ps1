a45cd6098158cfc414b5f8d4d1f4709ddbde622f
DDanii <ddani1995@gmail.com>
2026-07-24 19:39:46 +0200

    Delete Wyoming.Net.Satellite.App.Tizen directory

--- Wyoming.Net.Satellite.App.Tizen/Tizen/Scripts/log_server.ps1
+++
@@ -1,6 +0,0 @@
-$udp = New-Object System.Net.Sockets.UdpClient(5005)
-$ep = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any,0)
-while ($true) {
-  $data = $udp.Receive([ref]$ep)
-  [Text.Encoding]::UTF8.GetString($data)
-}
--- Wyoming.Net.Satellite.App.Tizen/Tizen/Scripts/resize_model.ps1
+++
@@ -1,2 +0,0 @@
-# This is used to convert the onnx version of melspectrogram into a statically shaped tflite model, which is what Tizen OneShot engine accepts
-# onnx2tf -i .\melspectrogram.onnx -o tf_mel --batch_size 1 --overwrite_input_shape "input:1,1760"
